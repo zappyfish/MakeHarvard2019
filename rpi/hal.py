29b5f5197564a1165b96d9675f31e1617797f19d
zappyfish <liamkelly2015@gmail.com>
2019-02-03 08:33:52 -0500

    vals

--- rpi/hal.py
+++ rpi/hal.py
@@ -16,8 +16,8 @@ class HardwareAbstractionLayer:
         angle_two_pin = 12
 
         self.z_motor = Motor(z_pin)
-        self.shoulder_motor = Motor(angle_one_pin, 40 * pi / 180)
-        self.elbow_motor = Motor(angle_two_pin, -80 * pi / 180)
+        self.shoulder_motor = Motor(angle_one_pin, 35 * pi / 180)
+        self.elbow_motor = Motor(angle_two_pin, 90 * pi / 180)
 
     def translate_instrument(self, delta_x, delta_y):
         desired = np.array([delta_x, delta_y])
